9181dd4d0285239fcf086b8a36b344818dbb712d
Viktorova Irina <victorova.irina@gmail.com>
2022-11-13 19:42:32 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 12800;
+        int miles = ticket / 20;
+        System.out.println("Бонусные мили: " + miles);
+    }
+}
